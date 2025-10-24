f7589d9df9f7d018a83d69b25df2c4e057ee76fe
melikw41 <melik.williams@yale.edu>
2025-10-23 21:34:25 -0400

    Melik Code

--- script.js
+++ script.js
@@ -1,37 +1,62 @@
-let msg = "-->";
-let cols = 10;
-let rows = 10;
+let currentPage = "start";
+let img;
+let button1, button2;
+
+function preload() {
+  img = loadImage('https://placehold.co/600x400');
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  textAlign(CENTER, CENTER);
+  textSize(20);
 
-  //we can divide the canvas width & height byt the number of columns and rows to get the dimensions of each grid.
-  grid_w = width / cols;
-  grid_h = height / rows;
-  textAlign(CENTER);
+ 
+  button1 = createButton('Go to Page 1');
+  button2 = createButton('Go to Page 2');
+
+
+  button1.position(windowWidth / 2 - 150, windowHeight / 2 - 100);
+  button2.position(windowWidth / 2 + 50, windowHeight / 2 - 100);
+
+ 
+  button1.mousePressed(() => {
+    currentPage = "page1";
+  });
+
+  button2.mousePressed(() => {
+    currentPage = "page2";
+  });
 }
 
 function draw() {
   background(220);
-  //let's start by drawing each column only
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      //we are going to use the grid_w to space out the columns,
-      //we drawing a square of dimensions grid_w and grid_h
-      let x = i * grid_w;
-      let y = j * grid_h;
-      let distance = dist(mouseX, mouseY,x, y); // Calculating distance of mouse to (x,y)
 
-      push();
-      translate(x, y);
-      rotate(distance/150);
-      textSize(map(distance,0,width,50,2))
-      //rect(0, 0, grid_w, grid_h);
-      text(msg, grid_w / 2, grid_h / 2);
-      // fill(i*20,j*20,200)
-      // rect(x,y,x+grid_w,y+grid_h)
-      // text(msg,x+grid_w/2,y+grid_h/2)
-      pop();
-    }
+  if (currentPage === "start") {
+    showStartPage();
+  } else if (currentPage === "page1") {
+    showPage("You are on Page 1. Something happens here.");
+  } else if (currentPage === "page2") {
+    showPage("You are on Page 2. Something else happens.");
   }
 }
+
+function showStartPage() {
+  image(img, 0, 0, width, height / 2);
+
+  fill(0);
+  text("You are at the beginning of your journey. What do you want to do?", width / 2, height / 2 + 100);
+
+  button1.show();
+  button2.show();
+}
+
+function showPage(message) {
+  button1.hide();
+  button2.hide();
+
+  background(200);
+  fill(0);
+  text(message, width / 2, height / 2);
+}
+

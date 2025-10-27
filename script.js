64ab719cc377fa4c49561bd27597004dd845c72e
kyezhng <itskaizhang@gmail.com>
2025-10-27 11:59:21 -0400

    push

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// // let scene = 0;
+// let chinese = 0
+// let mexican = 0;
+// let sweetgreen = 0;
+// let choice = "";
+
 const lunchButtons = document.querySelectorAll('.lunch-choices button');
 
 lunchButtons.forEach(button => {
@@ -8,11 +14,15 @@ lunchButtons.forEach(button => {
     // Hide all buttons
     lunchButtons.forEach(btn => btn.style.display = 'none');
 
-    // Optionally, show new choices
-    showNext1_C_Choices();
+    // Show next choices based on selection
+    if (choice === "chinese") {
+        showNext1_C_Choices();
+    } else if (choice === "mexican") {
+        showNext1_M_Choices();
+    }
+
   });
 });
-
 
 function showNext1_C_Choices() {
     const nextButtons = document.querySelectorAll('.scene1-C-choices button');
@@ -22,11 +32,19 @@ function showNext1_C_Choices() {
     });
 }
 
+function showNext1_M_Choices() {
+    const nextButtons = document.querySelectorAll('.scene1-M-choices button');
 
-///// CHINESE FOOD
+    nextButtons.forEach(button => {
+        button.style.display = 'flex'; //'inline-block'; // or 'block'
+    });
+}
+
+// CHINESE FOOD
+// Scene 1
 const scene1_C_Buttons = document.querySelectorAll('.scene1-C-choices button');
 
-    scene1_C_Buttons.forEach(button => {
+scene1_C_Buttons.forEach(button => {
     button.addEventListener('click', () => {
     choice = button.id;
     scene = 2;
@@ -34,15 +52,123 @@ const scene1_C_Buttons = document.querySelectorAll('.scene1-C-choices button');
     // Hide all buttons
     scene1_C_Buttons.forEach(btn => btn.style.display = 'none');
 
-    // Optionally, show new choices
-    showNext2_C_J_Choices();
+    // Show next scene choices depending on if junzi/chef jiang is selected
+    if (choice == "junzi") {
+        showNext2_C_J_Choices();
+    } else if (choice == "chef-jiang") {
+        showNext2_C_C_Choices();
+    }
   });
 });
 
+// Scene 2
 function showNext2_C_J_Choices() {
     const nextButtons = document.querySelectorAll('.scene2-C-J-choices button');
 
     nextButtons.forEach(button => {
-        button.style.display = 'flex'; //'inline-block'; // or 'block'
+        button.style.display = 'flex'; 
     });
 }
+
+function showNext2_C_C_Choices () {
+    const nextButtons = document.querySelectorAll('.scene2-C-C-choices button');
+
+    nextButtons.forEach(button => {
+        button.style.display = 'flex'; 
+    });
+}
+
+// Scene 3
+const scene2_C_J_Buttons = document.querySelectorAll('.scene2-C-J-choices button');
+const scene2_C_C_Buttons = document.querySelectorAll('.scene2-C-C-choices button');
+
+scene2_C_J_Buttons.forEach(button => {
+    button.addEventListener('click', () => {
+    choice = button.id;
+    scene = 3;
+    console.log('Hello')
+
+    // Hide all buttons
+    scene2_C_J_Buttons.forEach(btn => btn.style.display = 'none');
+  });
+}); 
+
+scene2_C_C_Buttons.forEach(button => {
+    button.addEventListener('click', () => {
+    choice = button.id;
+    scene = 4;
+
+    console.log('Hello')
+    // Hide all buttons
+    scene2_C_C_Buttons.forEach(btn => btn.style.display = 'none');
+  });
+});
+// END OF CHINESE FOOD
+
+/////// /////// /////// /////// /////// /////// /////// 
+
+// MEXICAN FOOD
+const scene1_M_Buttons = document.querySelectorAll('.scene1-M-choices button');
+
+scene1_M_Buttons.forEach(button => {
+    button.addEventListener('click', () => {
+    mexicanChoice = button.id;
+    scene = 2;
+    mexican = 1; //track Mexican
+
+    // Hide all buttons
+    scene1_M_Buttons.forEach(btn => btn.style.display = 'none');
+
+    // Show next scene choices depending on if junzi/chef jiang is selected
+    if (mexicanChoice == "birria") {
+        showNext2_M_B_Choices();
+    } else if (mexicanChoice == "fish") {
+        showNext2_M_F_Choices();
+    }
+  });
+});
+
+// Scene 2
+function showNext2_M_B_Choices() {
+    const nextButtons = document.querySelectorAll('.scene2-M-B-choices button');
+
+    nextButtons.forEach(button => {
+        button.style.display = 'flex'; 
+    });
+}
+
+function showNext2_M_F_Choices () {
+    const nextButtons = document.querySelectorAll('.scene2-M-F-choices button');
+
+    nextButtons.forEach(button => {
+        button.style.display = 'flex'; 
+    });
+}
+
+// Scene 3
+const scene2_M_B_Buttons = document.querySelectorAll('.scene2-M-B-choices button');
+const scene2_M_F_Buttons = document.querySelectorAll('.scene2-M-F-choices button');
+
+scene2_M_B_Buttons.forEach(button => {
+    button.addEventListener('click', () => {
+    mexicanChoice = button.id;
+    scene = 3;
+    console.log('Hello')
+
+    // Hide all buttons
+    scene2_M_B_Buttons.forEach(btn => btn.style.display = 'none');
+  });
+}); 
+
+scene2_C_C_Buttons.forEach(button => {
+    button.addEventListener('click', () => {
+    mexicanChoice = button.id;
+    scene = 3;
+
+    console.log('Hello')
+    // Hide all buttons
+    scene2_C_C_Buttons.forEach(btn => btn.style.display = 'none');
+  });
+});
+
+// END OF MEXICAN FOOD
--- sketch-2.js
+++ sketch-2.js
@@ -1,43 +1,94 @@
 let scene = 0;
-let chinese = 0
-let mexican = 0;
-let sweetgreen = 0;
 let choice = "";
+let mexicanChoice = "";
 let newFont;
+
 
 function preload() {
     newFont = loadFont('assets/MontaguSlab_48pt-Regular.ttf')
 }
 
+// function setup() {
+//     // Get the container size
+//     const container = document.getElementById('canvas-container');
+//     const canvas = createCanvas(container.offsetWidth, container.offsetHeight);
+//     canvas.parent('canvas-container'); // attach canvas to the div
+  
+//     textFont(newFont);
+//     textAlign(CENTER, CENTER);
+//     textSize(40);
+
+//     // Event listeners -- Will learn more about this I'm sure
+//     const lunchButtons = document.querySelectorAll('.lunch-choices button');
+
+//     lunchButtons.forEach(button => {
+//         button.addEventListener('click', () => {
+//             choice = button.id; // "chinese", "mexican", or "indian"
+//             scene = 1;
+//         });
+//     });
+
+//     const scene1_C_Buttons = document.querySelectorAll('.scene1-C-choices button');
+
+//     scene1_C_Buttons.forEach(button => {
+//         button.addEventListener('click', () => {
+//             choice = button.id;
+//             scene = 2;
+//         });
+//     })
+// }
+
 function setup() {
-    // Get the container size
     const container = document.getElementById('canvas-container');
     const canvas = createCanvas(container.offsetWidth, container.offsetHeight);
-    canvas.parent('canvas-container'); // attach canvas to the div
-  
+    canvas.parent('canvas-container');
+
     textFont(newFont);
     textAlign(CENTER, CENTER);
     textSize(40);
 
-    // Event listeners -- Will learn more about this I'm sure
+    // Lunch buttons
     const lunchButtons = document.querySelectorAll('.lunch-choices button');
-
-    lunchButtons.forEach(button => {
-        button.addEventListener('click', () => {
-            choice = button.id; // "chinese", "mexican", or "indian"
+    lunchButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            choice = btn.id; // "chinese" or "mexican"
             scene = 1;
+
+            // Hide all lunch buttons
+            lunchButtons.forEach(b => b.style.display = "none");
+
+            if (choice === "chinese") showNext1_C_Choices();
+            else if (choice === "mexican") showNext1_M_Choices();
         });
     });
 
+    // Scene 1 Chinese
     const scene1_C_Buttons = document.querySelectorAll('.scene1-C-choices button');
-
-    scene1_C_Buttons.forEach(button => {
-        button.addEventListener('click', () => {
-            choice = button.id;
+    scene1_C_Buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            choice = btn.id; // junzi or chef-jiang
             scene = 2;
+            scene1_C_Buttons.forEach(b => b.style.display = "none");
+
+            if (choice == "junzi") showNext2_C_J_Choices();
+            else if (choice == "chef-jiang") showNext2_C_C_Choices();
         });
-    })
+    });
+
+    // Scene 1 Mexican
+    const scene1_M_Buttons = document.querySelectorAll('.scene1-M-choices button');
+    scene1_M_Buttons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            mexicanChoice = btn.id; // birria or fish
+            scene = 2;
+            scene1_M_Buttons.forEach(b => b.style.display = "none");
+
+            if (mexicanChoice == "birria") showNext2_M_B_Choices();
+            else if (mexicanChoice == "fish") showNext2_M_F_Choices();
+        });
+    });
 }
+
 
 function draw() {
     background('#4e529a');
@@ -60,32 +111,44 @@ function draw() {
         text('So, what do you want for lunch?', width/2, height/2);
     } else if (scene == 1) {
         if (choice === "chinese") {
-            textAlign(CENTER);
             textSize(30);
             text('Which Chinese restaurant do you want to go to?', width/2, height/2);
         } else if (choice === "mexican") {
-            textAlign(CENTER);
             textSize(40);
             text('What kind of tacos are you getting?', width/2, height/2);
-        } else if (choice === "indian") {
-            textAlign(CENTER);
-            textSize(45);
-            text('When are you going to Sweetgreen?', width/2, height/2);
         }
     } else if (scene == 2) {
         if (choice == "junzi") {
-            textAlign(CENTER);
             textSize(25);
             text('It is the 10-year anniversary of Junzi, \nand everyone gets a free bowl. What is going in your bowl?', width/2, height/2);
-        } else if (choice == "mexican") {
-            textAlign(CENTER);
-            textSize(40);
-            text('What kind of tacos are you getting?', width/2, height/2);
-        } else if (choice === "indian") {
-            textAlign(CENTER);
-            textSize(45);
-            text('When are you going to Sweetgreen?', width/2, height/2);
+        } else if (choice == "chef-jiang") {
+            textSize(25);
+            text('You go eat at Chef Jiang and get an \nallergic reaction from the food. What do you do?', width/2, height/2);
+        } else if (mexicanChoice == "birria") {
+            textSize(25);
+            text('When you pick up your tacos, someone \nwaiting behind you wacks you with a baguette and \nsteals your taco. What do you do?', width/2, height/2);
+        } else if (mexicanChoice == "fish") {
+            textSize(30);
+            text('You order your fish tacos. A seagull approaches.', width/2, height/2);
         }
+    
+    } else if (scene == 3) {
+        textSize(40);
+        if (choice === "canto-chicken" || choice === "tofu") {
+            text('Fantastic choice!', width/2, height/2);
+        } else if (choice === "butt") {
+            textSize(30);
+            text('Your butt could not press the right number \non your phone, so you are dead now.', width/2, height/2);
+        } else if (choice === "waiter") {
+            textSize(30)
+            text('The waiter saw you and injected you \nwith the appropriate epipen. \nYou live to eat another day.', width/2, height/2);
+        } else if (mexicanChoice === "dine-together") {
+            textSize(25)
+            text('Wow. Your kindness got rewarded with a new friend. Congrats!', width/2, height/2);
+        }  else if (mexicanChoice === "run") {
+            textSize(25)
+            text('You run after that man and trip on a rock. \nYour taco gets stolen and you take quite a knock. \nYou just lost so much aura.', width/2, height/2);
+        } 
     }
 }
 

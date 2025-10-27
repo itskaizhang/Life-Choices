4beca8e2388ff8119803df9088427812780b9833
kyezhng <itskaizhang@gmail.com>
2025-10-27 09:26:55 -0400

    pushing

--- script.js
+++ script.js
@@ -9,13 +9,38 @@ lunchButtons.forEach(button => {
     lunchButtons.forEach(btn => btn.style.display = 'none');
 
     // Optionally, show new choices
-    showNextChoices();
+    showNext1_C_Choices();
   });
 });
 
 
-function showNextChoices() {
+function showNext1_C_Choices() {
     const nextButtons = document.querySelectorAll('.scene1-C-choices button');
+
+    nextButtons.forEach(button => {
+        button.style.display = 'flex'; //'inline-block'; // or 'block'
+    });
+}
+
+
+///// CHINESE FOOD
+const scene1_C_Buttons = document.querySelectorAll('.scene1-C-choices button');
+
+    scene1_C_Buttons.forEach(button => {
+    button.addEventListener('click', () => {
+    choice = button.id;
+    scene = 2;
+
+    // Hide all buttons
+    scene1_C_Buttons.forEach(btn => btn.style.display = 'none');
+
+    // Optionally, show new choices
+    showNext2_C_J_Choices();
+  });
+});
+
+function showNext2_C_J_Choices() {
+    const nextButtons = document.querySelectorAll('.scene2-C-J-choices button');
 
     nextButtons.forEach(button => {
         button.style.display = 'flex'; //'inline-block'; // or 'block'
--- sketch-2.js
+++ sketch-2.js
@@ -22,27 +22,21 @@ function setup() {
     // Event listeners -- Will learn more about this I'm sure
     const lunchButtons = document.querySelectorAll('.lunch-choices button');
 
-    // lunchButtons.forEach(button => {
-    //     button.addEventListener('click', () => {
-    //         if (button.id === 'chinese') {
-    //             chinese = chinese + 1;
-    //             scene = 1;
-    //         } else if (button.id === 'mexican') {
-    //             mexican++;
-    //             scene = 1;
-    //         } else if (button.id === 'indian') {
-    //             currentImage = imgIndian;
-    //             // ellipseColor = color(255, 100, 0);
-    //         }
-    //     });
-    // });
-
     lunchButtons.forEach(button => {
         button.addEventListener('click', () => {
             choice = button.id; // "chinese", "mexican", or "indian"
             scene = 1;
         });
     });
+
+    const scene1_C_Buttons = document.querySelectorAll('.scene1-C-choices button');
+
+    scene1_C_Buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            choice = button.id;
+            scene = 2;
+        });
+    })
 }
 
 function draw() {
@@ -70,12 +64,50 @@ function draw() {
             textSize(30);
             text('Which Chinese restaurant do you want to go to?', width/2, height/2);
         } else if (choice === "mexican") {
+            textAlign(CENTER);
+            textSize(40);
             text('What kind of tacos are you getting?', width/2, height/2);
         } else if (choice === "indian") {
+            textAlign(CENTER);
+            textSize(45);
+            text('When are you going to Sweetgreen?', width/2, height/2);
+        }
+    } else if (scene == 2) {
+        if (choice == "junzi") {
+            textAlign(CENTER);
+            textSize(25);
+            text('It is the 10-year anniversary of Junzi, \nand everyone gets a free bowl. What is going in your bowl?', width/2, height/2);
+        } else if (choice == "mexican") {
+            textAlign(CENTER);
+            textSize(40);
+            text('What kind of tacos are you getting?', width/2, height/2);
+        } else if (choice === "indian") {
+            textAlign(CENTER);
+            textSize(45);
             text('When are you going to Sweetgreen?', width/2, height/2);
         }
     }
 }
+
+
+
+// CODE SNIPPETS
+
+   // lunchButtons.forEach(button => {
+    //     button.addEventListener('click', () => {
+    //         if (button.id === 'chinese') {
+    //             chinese = chinese + 1;
+    //             scene = 1;
+    //         } else if (button.id === 'mexican') {
+    //             mexican++;
+    //             scene = 1;
+    //         } else if (button.id === 'indian') {
+    //             currentImage = imgIndian;
+    //             // ellipseColor = color(255, 100, 0);
+    //         }
+    //     });
+    // });
+
 
  // document.getElementById('mexican').addEventListener('click', () => {
     //     ellipseColor = color(0, 255, 0);
